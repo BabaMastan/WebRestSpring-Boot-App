45d5d1f85904f6ff8cfb5a4ded3dfe8081c70327
MastanShaik <mastan30@gmail.com>
2017-04-26 14:14:53 -0400

    Add files via upload

---
+++ src/main/java/com/project/WebRestAppApplication.java
@@ -0,0 +1,12 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRestAppApplication.class, args);
+	}
+}

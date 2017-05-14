c3e6cfb0337f6047dfa359897030a091e03bac4d
GAMER <GAMER@DESKTOP-F6ISIHS>
2017-05-14 01:11:04 -0400

    added css and js

--- src/main/java/com/project/controller/SafeController.java
+++ src/main/java/com/project/controller/SafeController.java
@@ -1,5 +1,16 @@
 package com.project.controller;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class SafeController {
+	@RequestMapping("/safe")
+	public String index(Model model) {
+    	return "index";
+    }
 
 }

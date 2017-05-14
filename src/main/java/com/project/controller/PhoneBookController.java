eb629a12a4e533b71e806e67ee63dad435f753a4
GAMER <GAMER@DESKTOP-F6ISIHS>
2017-05-13 23:30:57 -0400

    Added logger

--- src/main/java/com/project/controller/PhoneBookController.java
+++ src/main/java/com/project/controller/PhoneBookController.java
@@ -2,8 +2,20 @@ package com.project.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 public class PhoneBookController {
 	private static final Logger logger = LoggerFactory.getLogger(PhoneBookController.class);
+	
+	@RequestMapping(value="/")
+	public String info()
+	{
+		logger.debug("This is a test for logger creation");
+		return "This app is for Phone and Password Safe";
+		
+	}
 	
 }
